e99f94136361e17304309efceae200bffa3ba9ea
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-12-17 11:05:09 -0700

    branch three finished

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ class App extends Component {
   }
   getGreenVehicles() {
     axios.get(this.state.baseUrl + '?color=green')
-      .then(res => this.setState({ vehicles: res.data.vehicles }))
+      .then(res => this.setState({ vehicles: res.data }))
   }
   getFords() {
-    axios.get(this.state.baseUrl + '?make=tesla')
+    axios.get(this.state.baseUrl + '?make=ford')
       .then(res => this.setState({ vehicles: res.data }))
   }
 
@@ -45,7 +45,7 @@ class App extends Component {
           <Button variant='contained' onClick={() => this.getGreenVehicles()}>Green Vehicles</Button>
         </p>
         <p>
-          <Button variant='contained' onClick={() => this.getFords()}>Tesla Vehicles</Button>
+          <Button variant='contained' onClick={() => this.getFords()}>Ford Vehicles</Button>
         </p>
         <hr />
         <h4>Vehicles:</h4>
